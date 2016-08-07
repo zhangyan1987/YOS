317b978ea97e73d62e97d5eaffedff04c987e4ca
SChen <SChen@SChen-PC>
2016-08-07 08:40:04 +0800

    init version

---
+++ yos/src/main/java/yos/actions/LoginAction.java
@@ -0,0 +1,17 @@
+package yos.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6150544976038858479L;
+
+	@Override
+	public String execute() throws Exception {
+		return SUCCESS;
+	}
+
+}
